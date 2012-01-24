d0c41900a6a9fa9f0f5c3cbc212450c7f19d0c63
lycantrophe <wolfhood@gmail.com>
2012-01-24 11:26:43 +0100

    Implemented propertyChangeListeners.

--- src/oving2/PersonPanel.java
+++ src/oving2/PersonPanel.java
@@ -5,6 +5,8 @@ import java.awt.GridBagLayout;
 import java.awt.TextField;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.beans.PropertyChangeEvent;
+import java.beans.PropertyChangeListener;
 import javax.swing.*;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
@@ -17,7 +19,7 @@ import javax.swing.event.ChangeListener;
  *
  * @author lycantrophe
  */
-public class PersonPanel extends JPanel {
+public class PersonPanel extends JPanel implements PropertyChangeListener {
 
     Person person;
     javax.swing.JTextField NamePropertyComponent;
@@ -25,48 +27,45 @@ public class PersonPanel extends JPanel {
     javax.swing.JTextField DateOfBirthPropertyComponent;
     javax.swing.JComboBox GenderPropertyComponent;
     javax.swing.JSlider HeightPropertyComponent;
-    
+
     public PersonPanel() {
 
-       // JPanel pane = new JPanel(new GridBagLayout());
-        //JPanel pane = new JPanel();
         this.setLayout(new GridBagLayout());
         GridBagConstraints c = new GridBagConstraints();
         c.fill = GridBagConstraints.HORIZONTAL;
-        
+
         myChangeListener cl = new myChangeListener();
         myActionListener ae = new myActionListener();
-        
+
         person = new Person("Dummy");
 
         NamePropertyComponent = new javax.swing.JTextField("", 50);
         NamePropertyComponent.addActionListener(ae);
         NamePropertyComponent.setName("NamePropertyComponent");
         c.gridy = 0;
-        add(new JLabel("name:"),c);
-        add(NamePropertyComponent,c);
-        
+        add(new JLabel("name:"), c);
+        add(NamePropertyComponent, c);
 
         EmailPropertyComponent = new javax.swing.JTextField("", 50);
         EmailPropertyComponent.addActionListener(ae);
         EmailPropertyComponent.setName("EmailPropertyComponent");
         c.gridy = 1;
-        add(new JLabel("email:"),c);
-        add(EmailPropertyComponent,c);
+        add(new JLabel("email:"), c);
+        add(EmailPropertyComponent, c);
 
         DateOfBirthPropertyComponent = new JTextField("", 10);
         DateOfBirthPropertyComponent.addActionListener(ae);
         DateOfBirthPropertyComponent.setName("DateOfBirthPropertyComponent");
         c.gridy = 2;
-        add(new JLabel("gender:"),c);
-        add(DateOfBirthPropertyComponent,c);
-        
+        add(new JLabel("gender:"), c);
+        add(DateOfBirthPropertyComponent, c);
+
         GenderPropertyComponent = new JComboBox(Person.Gender.values());
         GenderPropertyComponent.addActionListener(ae);
         GenderPropertyComponent.setName("GenderPropertyComponent");
         c.gridy = 3;
-        add(new JLabel("date of birth:"),c);
-        add(GenderPropertyComponent,c);
+        add(new JLabel("date of birth:"), c);
+        add(GenderPropertyComponent, c);
 
         HeightPropertyComponent = new JSlider(100, 200);
         HeightPropertyComponent.addChangeListener(cl);
@@ -76,51 +75,87 @@ public class PersonPanel extends JPanel {
         HeightPropertyComponent.setMinorTickSpacing(3);
         HeightPropertyComponent.setName("HeightPropertyComponent");
         c.gridy = 4;
-        add(new JLabel("height:"),c);
-        add(HeightPropertyComponent,c);
-        
+        add(new JLabel("height:"), c);
+        add(HeightPropertyComponent, c);
+
         setVisible(true);
-        
+
     }
 
     private class myActionListener implements ActionListener {
+
         @Override
         public void actionPerformed(ActionEvent ae) {
             if (ae.getSource() == NamePropertyComponent && !NamePropertyComponent.getText().equals("")) {
-                    person.setName(NamePropertyComponent.getText());
+                person.setName(NamePropertyComponent.getText());
             } else if (ae.getSource() == EmailPropertyComponent && !EmailPropertyComponent.getText().equals("")) {
-                    person.setEmail(EmailPropertyComponent.getText());
+                person.setEmail(EmailPropertyComponent.getText());
             } else if (ae.getSource() == DateOfBirthPropertyComponent && !DateOfBirthPropertyComponent.getText().equals("")) {
-                    person.setDateOfBirth(DateOfBirthPropertyComponent.getText());
-            }  else if (ae.getSource() == GenderPropertyComponent) {
-                person.setGender((Person.Gender)GenderPropertyComponent.getSelectedItem());
+                person.setDateOfBirth(DateOfBirthPropertyComponent.getText());
+            } else if (ae.getSource() == GenderPropertyComponent) {
+                person.setGender((Person.Gender) GenderPropertyComponent.getSelectedItem());
             }
         }
     }
 
     public void setModel(Person person) {
         this.person = person;
+        person.addPropertyChangeListener(this);
     }
 
     public Person getModel() {
         return person;
     }
-    
+
     private class myChangeListener implements ChangeListener {
+
         @Override
-        public void stateChanged(ChangeEvent ce){
+        public void stateChanged(ChangeEvent ce) {
             int hval = HeightPropertyComponent.getValue();
-            if (hval != person.getHeight()) { person.setHeight(hval); }
+            if (hval != person.getHeight()) {
+                person.setHeight(hval);
+            }
         }
     }
-    
-        public static void main(String[] args){
-        
+
+    @Override
+    public void propertyChange(PropertyChangeEvent evt) {
+        if (evt.getPropertyName().equals("NAME")) {
+            NamePropertyComponent.setText(person.getName());
+        }
+        if (evt.getPropertyName().equals("EMAIL")) {
+            EmailPropertyComponent.setText(person.getEmail());
+        }
+        if (evt.getPropertyName().equals("HEIGHT")) {
+            DateOfBirthPropertyComponent.setText(person.getDateOfBirth());
+        }
+        if (evt.getPropertyName().equals("GENDER")) {
+            GenderPropertyComponent.setSelectedItem(person.getGender());
+        }
+        if (evt.getPropertyName().equals("HEIGHT")) {
+            HeightPropertyComponent.setValue(person.getHeight());
+        }
+    }
+
+    public static void main(String[] args) {
+
         JFrame canvas = new JFrame();
-        PersonPanel pp = new PersonPanel();
+        JFrame passiveCanvas = new JFrame();
         
+        PersonPanel pp = new PersonPanel();
+        PersonPanel passivepp = new PersonPanel();
+        Person person = new Person( "Annemerete" );
+        
+        passivepp.setModel( person );
+        pp.setModel(person);
+
         canvas.add(pp);
         canvas.pack();
         canvas.setVisible(true);
+        
+        
+        passiveCanvas.add(passivepp);
+        passiveCanvas.pack();
+        passiveCanvas.setVisible(true);
     }
 }
